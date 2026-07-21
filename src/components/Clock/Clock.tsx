d8565b3b5f86100d7d9624152ee94908d35c732c
ismaelmarot <43220852+ismaelmarot@users.noreply.github.com>
2026-07-21 09:12:09 -0300

    refact

--- src/components/Clock/Clock.tsx
+++ src/components/Clock/Clock.tsx
@@ -1,20 +1,20 @@
-import React, { useState, useEffect } from 'react';
-import { ClockContainer, Column, NumbersList, Cell, Colon } from './Clock.styled';
-import { DIGITS } from '../../constants/digits';
+import React, { useState, useEffect } from 'react'
+import { ClockContainer, Column, NumbersList, Cell, Colon } from './Clock.styled'
+import { DIGITS } from '../../constants/digits'
 
-const CELL_HEIGHT = 50;
+const CELL_HEIGHT = 50
 
 const Clock: React.FC = () => {
-  const [time, setTime] = useState(new Date());
+  const [time, setTime] = useState(new Date())
 
   useEffect(() => {
-    const interval = setInterval(() => setTime(new Date()), 1000);
-    return () => clearInterval(interval);
-  }, []);
+    const interval = setInterval(() => setTime(new Date()), 1000)
+    return () => clearInterval(interval)
+  }, [])
 
-  const hours = time.getHours().toString().padStart(2, '0');
-  const minutes = time.getMinutes().toString().padStart(2, '0');
-  const seconds = time.getSeconds().toString().padStart(2, '0');
+  const hours = time.getHours().toString().padStart(2, '0')
+  const minutes = time.getMinutes().toString().padStart(2, '0')
+  const seconds = time.getSeconds().toString().padStart(2, '0')
 
   const digits = [
     { col: DIGITS.H1, val: hours[0] },
@@ -23,13 +23,13 @@ const Clock: React.FC = () => {
     { col: DIGITS.M2, val: minutes[1] },
     { col: DIGITS.S1, val: seconds[0] },
     { col: DIGITS.S2, val: seconds[1] },
-  ];
+  ]
 
   return (
     <ClockContainer>
       {digits.map((digit, i) => {
-        const index = digit.col.findIndex((n) => n === digit.val);
-        const offset = (-index * CELL_HEIGHT) + 225;
+        const index = digit.col.findIndex((n) => n === digit.val)
+        const offset = (-index * CELL_HEIGHT) + 225
 
         return (
           <React.Fragment key={i}>
@@ -47,7 +47,7 @@ const Clock: React.FC = () => {
         );
       })}
     </ClockContainer>
-  );
-};
+  )
+}
 
-export default Clock;
+export default Clock
